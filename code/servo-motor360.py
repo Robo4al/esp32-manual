a7071a25d2679d7d699e1bdf12e0abef9e82a34f
Kessia Carvalho <50149818+kessiac@users.noreply.github.com>
2020-05-21 14:37:10 -0300

    Update servo-motor360.py

--- code/servo-motor360.py
+++ code/servo-motor360.py
@@ -1,7 +1,7 @@
 import machine, time
 from machine import Pin, PWM
 
-servo = machine.PWM(Pin(2), freq=200, duty=550)
+servo = machine.PWM(Pin(5), freq=200, duty=550)
 time.sleep(5)
 servo.deinit()
 
