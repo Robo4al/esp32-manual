d06810ac09a312f299ead88399c5dbee1147e604
Kessia Carvalho <50149818+kessiac@users.noreply.github.com>
2020-05-21 14:39:00 -0300

    code

--- code/servo-joystick.py
+++ code/servo-joystick.py
@@ -10,15 +10,15 @@ while True:
   leitura = leitura * 3.3 / 4095
   
   if leitura == 0.0 or leitura == 00.0 or leitura == 0.00:
-    servo = PWM(Pin(2), freq=100)
+    servo = PWM(Pin(4), freq=100)
     servo.duty(250)
     
   elif leitura == 3.3:
-    servo = PWM(Pin(2), freq=100)
+    servo = PWM(Pin(4), freq=100)
     servo.duty(40)
 
   else:
-    servo = PWM(Pin(2), freq=100)
+    servo = PWM(Pin(4), freq=100)
     servo.duty(0)
 
 
